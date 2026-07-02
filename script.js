b85f6ed061fcac98b08daed1535382546ada0603
oliffiageo <larenszaoliffia@gmail.com>
2026-07-02 22:05:09 +0700

    Add smooth scrolling functionality on button click

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Script akan kita isi nanti
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".button");
+
+  if (btn) {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const tujuan = document.querySelector("#acara");
+
+      if (tujuan) {
+        tujuan.scrollIntoView({
+          behavior: "smooth"
+        });
+      }
+    });
+  }
+});
