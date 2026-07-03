96a644bb93d6bc8365a94b43e1d5fdf4857eb7dc
oliffiageo <larenszaoliffia@gmail.com>
2026-07-03 18:43:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+const flowerContainer = document.createElement("div");
+flowerContainer.className = "flower-container";
+document.body.appendChild(flowerContainer);
+
+function createFlower() {
+    const flower = document.createElement("div");
+    flower.className = "flower";
+
+    const flowers = ["🌸","🤍","🌼","🌷"];
+    flower.innerHTML = flowers[Math.floor(Math.random() * flowers.length)];
+
+    flower.style.left = Math.random() * 100 + "vw";
+    flower.style.animationDuration = (5 + Math.random() * 5) + "s";
+    flower.style.fontSize = (18 + Math.random() * 18) + "px";
+
+    flowerContainer.appendChild(flower);
+
+    setTimeout(() => {
+        flower.remove();
+    }, 10000);
+}
+
+setInterval(createFlower, 600);
