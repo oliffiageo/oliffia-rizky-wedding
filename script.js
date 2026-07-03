6701664fc6bb2227130525bfc22b204849cfe434
oliffiageo <larenszaoliffia@gmail.com>
2026-07-03 18:55:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector(".button");
+
+  const btn = document.querySelector(".rsvp-btn");
 
   if (btn) {
     btn.addEventListener("click", function (e) {
       e.preventDefault();
 
-      const tujuan = document.querySelector("#acara");
+      const tujuan = document.querySelector("#thanks");
 
       if (tujuan) {
         tujuan.scrollIntoView({
@@ -14,27 +15,29 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
-});
-const flowerContainer = document.createElement("div");
-flowerContainer.className = "flower-container";
-document.body.appendChild(flowerContainer);
 
-function createFlower() {
+  const flowerContainer = document.createElement("div");
+  flowerContainer.className = "flower-container";
+  document.body.appendChild(flowerContainer);
+
+  function createFlower() {
     const flower = document.createElement("div");
     flower.className = "flower";
 
-    const flowers = ["🌸","🤍","🌼","🌷"];
+    const flowers = ["🌸","🤍","🌼","🌷","🌹","✨"];
     flower.innerHTML = flowers[Math.floor(Math.random() * flowers.length)];
 
     flower.style.left = Math.random() * 100 + "vw";
     flower.style.animationDuration = (5 + Math.random() * 5) + "s";
-    flower.style.fontSize = (18 + Math.random() * 18) + "px";
+    flower.style.fontSize = (18 + Math.random() * 20) + "px";
 
     flowerContainer.appendChild(flower);
 
     setTimeout(() => {
-        flower.remove();
+      flower.remove();
     }, 10000);
-}
+  }
 
-setInterval(createFlower, 600);
+  setInterval(createFlower, 600);
+
+});
